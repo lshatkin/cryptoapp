57dc6167d0fe4c087ea880fcd00a6c229cb24e44
Lloyd Shatkin <lshatkin@umich.edu>
2018-01-28 15:46:32 -0500

    edits in formatting to coin_market_cap.py' '

--- python_scripts/coin_market_cap.py
+++ python_scripts/coin_market_cap.py
@@ -3,13 +3,43 @@ import json
 import pandas as pd 
 
 
+def format_entry(entry):
+    symbol = entry['symbol']
+    name = entry['name']
+    if not entry['price_btc']:
+        price_btc = 'Unknown'
+    else:
+        price_btc = entry['price_btc'] + ' BTC'
+    if not entry['price_usd']:
+        price_usd = 'Unknown'
+    else:
+        price_usd = '$' + entry['price_usd'][0:4]
+    if not (entry['percent_change_1h']):
+        percent_change_1h = 'Unknown'
+    else:
+        percent_change_1h = entry['percent_change_1h'] + '%'
+    if not entry['percent_change_24h']:
+        percent_change_24h = 'Unknown'
+    else:
+        percent_change_24h = entry['percent_change_24h'] + '%'
+    if not entry['24h_volume_usd']:
+        volume_usd_24h = 'Unknown'
+    else:
+        volume_usd_24h = "{:,}".format(float(entry['24h_volume_usd']))
+    if not entry['market_cap_usd']:
+        market_cap_usd = 'Unknown'
+    else:
+        market_cap_usd = '$' + "{:,}".format(float(entry['market_cap_usd']))
+    data_entry = [symbol, name, price_btc, price_usd, percent_change_1h,
+                    percent_change_24h, volume_usd_24h, market_cap_usd]
+    return data_entry
+
 
 def coinMarketCap():
     df = pd.DataFrame(columns = ['symbol', 'name', 'price_btc',
                                     'price_usd', 'percent_change_1h',
-                                    'percent_change_7d', 'volume_usd_24h',
-                                    'market_cap_usd', 'max_supply', 
-                                    'available_supply', 'total_supply'])
+                                    'percent_change_24h', 'volume_usd_24h',
+                                    'market_cap_usd'])
                                    
 
     response = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=0')
@@ -19,12 +49,7 @@ def coinMarketCap():
     for entry in response_json:
         print(count, "/", len(response_json))
         count += 1
-        data_entry = [entry['symbol'], entry['name'],
-                        entry['price_btc'], entry['price_usd'],
-                        entry['percent_change_1h'], entry['percent_change_7d'],
-                        entry['24h_volume_usd'], entry['market_cap_usd'],
-                        entry['max_supply'], entry['available_supply'],
-                        entry['total_supply']]
+        data_entry = format_entry(entry)
         df.loc[len(df.index)] = data_entry
     return df
 
